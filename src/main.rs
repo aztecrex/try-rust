545ad1ecf3d74dea474755562f7d749f084c3631
Greg Wiley <aztec.rex@jammm.com>
2017-05-06 12:55:31 -0700

    mutable borrow

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn try_borrow() -> i32 {
+    let mut x: i32 = 150;
+    {
+        let y = &mut x;
+        *y = *y + 100;
+    }
+    x
 }
 
-
+fn main() {
+    let x = try_borrow();
+    println!("Hello, {}", x);
+}
