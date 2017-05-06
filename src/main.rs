e5d621027eab506db092661f5019588aac28725c
Greg Wiley <aztec.rex@jammm.com>
2017-05-06 14:40:16 -0700

    try more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+
+fn try_temporary_mutability() {
+
+    struct Thing {
+        x: i32,
+        y: i32,
+    }
+
+    let mut t = Thing{ x: 0, y: 0};
+
+    t.x = 7;
+
+    let t = t;
+
+    println!("thing {}, {}",t.x,t.y);
+
+
+}
+
+fn try_ref() {
+    let x: &i32 = &12;
+    println!("{}",x);
+}
+
 fn try_borrow() -> i32 {
     let mut x: i32 = 150;
     {
@@ -8,6 +32,8 @@ fn try_borrow() -> i32 {
 }
 
 fn main() {
+    try_ref();
+    try_temporary_mutability();
     let x = try_borrow();
     println!("Hello, {}", x);
 }
